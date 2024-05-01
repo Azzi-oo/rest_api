b2a83f977b43b3a24218f381b708dabf15541d23
Azzi-oo <yanberdin.az@yandex.ru>
2024-05-01 23:15:12 +0500

    add new chnages

---
+++ drfsite/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from general.views import *
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'women', WomenViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
+    # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
+    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
+]
---
+++ general/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from general.models import Women
+
+# Register your models here.
+admin.site.register(Women)
